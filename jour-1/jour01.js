4e62f7320ce98c4858ae1f75fbcba862b04f87cc
unknown <jadisnou@gmail.com>
2021-03-08 16:03:31 +0100

    exercices javascript

---
+++ jour-1/jour01.js
@@ -0,0 +1,22 @@
+// - 01 Hello World
+console.log("Hello World !");
+
+// - 02 String
+var test;
+test = "My name is Ikram";
+console.log(test);
+
+// - 03 Concatenation
+var name;
+name = "Msadak";
+console.log("Nice to meet you "+ name );
+
+// - 04 String Length
+var testLength;
+testLength = "I'm very long !";
+console.log(testLength);
+
+// - 05 Replace
+var food = "Croissant is meh";
+food=food.replace("meh","so good");
+console.log(food)
