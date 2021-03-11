056bb9e1e7d2224e583d930f1098b149d39b80d8
unknown <jadisnou@gmail.com>
2021-03-11 17:12:53 +0100

    exercice4

---
+++ jour-4/exercice4.js
@@ -0,0 +1,18 @@
+function compare(num1, num2) {
+
+    if(num1 > num2){
+
+        console.log("num1 is bigger!")
+    }
+    else if(num1 < num2){
+        console.log("num2 is bigger!")
+    }
+     else {
+        console.log("both are the same!")
+
+    }
+
+
+
+}
+compare(6,4);
