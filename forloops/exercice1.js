06b1469e2a86047b41e58cd522a606326e618981
unknown <jadisnou@gmail.com>
2021-03-12 17:28:40 +0100

    exercice3

---
+++ forloops/exercice1.js
@@ -0,0 +1,23 @@
+var num1 = 5;
+var num2 = 10;
+var result = 0;
+
+for (var i = num1; i <= num2; i++) {
+    result = result + i * i;
+}
+
+console.log(result);
+
+
+
+
+// function Numbers(num1,num2) {
+//     var sum = 0
+//     for (var i = num1; i <= num2; i++) {
+//         sum = sum + i * i;
+//     }
+// return sum;
+
+// }
+
+// console.log(Numbers(5,10));
---
+++ forloops/exercice2.js
@@ -0,0 +1,15 @@
+var num1 = 100;
+var num2 = 1000;
+var conter = 0;
+
+for (var i = num1; i <= num2; i++) {
+    if (i % 7 === 0 ){
+        conter++ //conter = conter + 1
+    }
+
+}
+console.log(conter);
+
+
+
+
