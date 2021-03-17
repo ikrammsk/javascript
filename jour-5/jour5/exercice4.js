c74a8445284d917ec6ee15dbb5cca4b64c90d50b
unknown <jadisnou@gmail.com>
2021-03-17 22:35:57 +0100

    devoirs

--- jour-5/jour5/exercice4.js
+++ jour-5/jour5/exercice4.js
@@ -1,1 +1,30 @@
-var mysteryNum
+var mysteryNum = Math.floor(Math.random() * (1 - 100 + 1) + 100);
+var number = 0
+var prompt = require("prompt");
+
+prompt.start();
+
+function play() {
+    prompt.get ( { description: "'Quel est le nombre mystère ?'" }, function (mysteryNum, number) {
+        if (mysteryNum = NaN) {
+            console.log("error!");
+        }
+        else if (mysteryNum < number) {
+            play
+            console.log("C'est plus ! ")
+        }
+        else if (mysteryNum > number) {
+            play
+            console.log("C'est moins ! ")
+
+        }
+        else if (mysteryNum === number) {
+            play
+            console.log("Bravo ! ")
+
+        }
+
+    });
+}
+
+play()
