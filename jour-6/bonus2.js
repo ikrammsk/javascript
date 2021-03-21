10d2a6fa50d4e03fd990f09e5129901918637136
unknown <jadisnou@gmail.com>
2021-03-21 22:25:36 +0100

    devoirs

--- jour-6/bonus2.js
+++ jour-6/bonus2.js
@@ -4,15 +4,15 @@ var moment = require('moment');
 
 function calculateAge(birthDate) {
 
-    var birthDate = moment(new Date("1990/02/14"));
-    var todayDate = moment(new Date());
+    // var birthDate = moment(new Date("1990/02/14"));
+    // var todayDate = moment(new Date());
     
 
-    console.log(todayDate.diff(birthDate, "years"));      
+    // console.log(todayDate.diff(birthDate, "years"));      
 
 
-
-
+    var myAge = moment(birthDate,"YYYYMMDD").fromNow("yy")
+    console.log("You have",myAge);
 
 }
-calculateAge();
+calculateAge("1990-02-14");
