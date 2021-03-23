d3d13a6e26f94b63a4435721dfb0aaeb76bd6e95
unknown <jadisnou@gmail.com>
2021-03-23 14:51:29 +0100

    exos

--- jour-7/exo6.js
+++ jour-7/exo6.js
@@ -1,4 +1,4 @@
-var cake = [
+var cakes = [
 	{
 		name: "cake",
 		flavor: "vanilla",
@@ -26,3 +26,34 @@ var cake = [
 	},
 ]
 
+// var allCake = cake.filter(function (cake2) {
+// 	cake2.status = "sold out"
+// 	return (cake2.flavor === "chocolate")
+
+
+// });
+
+// console.log(allCake)
+
+
+var myCakesFavorite = cakes.map(function (cake) {
+
+	cake.flavor === "chocolate"
+
+	return cake;
+
+
+
+});
+
+// console.log(myCakesFavorite)
+
+var myCakesFavoriteList = cakes.filter(function (cake) {
+	cake.status = "sold out"
+	return (cake.flavor === "chocolate")
+
+
+});
+
+
+console.log(myCakesFavoriteList)
